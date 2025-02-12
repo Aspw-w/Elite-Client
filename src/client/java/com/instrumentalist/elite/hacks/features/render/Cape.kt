f81adafb33b34ed5406d63b070495b1b5ff813ab
Aspw <75427057+Aspw-w@users.noreply.github.com>
2025-02-12 20:37:26 +0900

    Cape Overrider

--- src/client/java/com/instrumentalist/elite/hacks/features/render/Cape.kt
+++ src/client/java/com/instrumentalist/elite/hacks/features/render/Cape.kt
@@ -2,9 +2,14 @@ package com.instrumentalist.elite.hacks.features.render
 
 import com.instrumentalist.elite.hacks.Module
 import com.instrumentalist.elite.hacks.ModuleCategory
+import com.instrumentalist.elite.utils.value.BooleanValue
 import org.lwjgl.glfw.GLFW
 
 class Cape : Module("Cape", ModuleCategory.Render, GLFW.GLFW_KEY_UNKNOWN, true, true) {
+    companion object {
+        @Setting
+        val overrideOriginalCape = BooleanValue("Override Original Cape", true)
+    }
 
     override fun onDisable() {}
     override fun onEnable() {}
